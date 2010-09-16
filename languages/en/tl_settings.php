0c99795d2a18cdc41e644e92417ac70232666e7f
Красимир Беров <k.berov@gmail.com>
2010-09-16 21:18:17 +0300

    [#1 Whitespaces] removed whitespaces at EOF

--- languages/en/tl_settings.php
+++ languages/en/tl_settings.php
@@ -26,4 +26,4 @@ $GLOBALS['TL_LANG']['tl_settings']['i18nl10n_languages'] =
 $GLOBALS['TL_LANG']['tl_settings']['i18nl10n_default_language'] = 
     array('Default Language', 'Enter a default language for your pages. '
           .'You should allways have a content element in this language.');
-?>                                          
+?>   
