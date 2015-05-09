5cd6523322b32e2e013a00b776ed62840e5da16b
Patric Eberle <line-in@derverstaerker.ch>
2015-05-09 21:45:42 +0200

    Version update #61

--- src/system/modules/i18nl10n/pages/PageI18nl10nRegular.php
+++ src/system/modules/i18nl10n/pages/PageI18nl10nRegular.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2014-2015 Verstärker, Patric Eberle
  * @author      Patric Eberle <line-in@derverstaerker.ch>
  * @package     i18nl10n pages
- * @version     1.2.1
+ * @version     1.3.6
  * @license     LGPLv3 http://www.gnu.org/licenses/lgpl-3.0.html
  */
 
