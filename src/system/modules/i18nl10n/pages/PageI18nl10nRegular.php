e347771f0a5599e6e1be2156f9c5ebdfccdb5b46
Patric Eberle <line-in@derverstaerker.ch>
2015-07-24 15:59:56 +0200

    Rasing version number to 1.5.1

--- src/system/modules/i18nl10n/pages/PageI18nl10nRegular.php
+++ src/system/modules/i18nl10n/pages/PageI18nl10nRegular.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2014-2015 Verstärker, Patric Eberle
  * @author      Patric Eberle <line-in@derverstaerker.ch>
  * @package     i18nl10n pages
- * @version     1.4.0
+ * @version     1.5.1
  * @license     LGPLv3 http://www.gnu.org/licenses/lgpl-3.0.html
  */
 
