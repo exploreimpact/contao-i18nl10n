cf1487c66207f02e03e018e2af36a065bfa065f8
Thiemo Kunze <kunze@wangaz.com>
2016-06-29 10:02:53 +0200

    adding support for link_name insert tag

--- src/system/modules/i18nl10n/classes/I18nl10n.php
+++ src/system/modules/i18nl10n/classes/I18nl10n.php
@@ -575,13 +575,20 @@ class I18nl10n extends \Controller
 
         $arrArguments = explode('::', $strTag);
 
-        if ($arrArguments[0] === 'i18nl10n' && $arrArguments[1] === 'link') {
-            $objNextPage = I18nl10n::getInstance()->findL10nWithDetails($arrArguments[2], $GLOBALS['TL_LANGUAGE']);
+        if ($arrArguments[0] === 'i18nl10n')
+        {
+	        $objNextPage = I18nl10n::getInstance()->findL10nWithDetails($arrArguments[2], $GLOBALS['TL_LANGUAGE']);
+        }
+        else
+        {
+	        return false;
+        }
 
-            if ($objNextPage === null) {
-                return false;
-            }
+		if ($objNextPage === null) {
+            return false;
+        }
 
+        if ($arrArguments[1] === 'link') {
             switch ($objNextPage->type) {
                 case 'redirect':
 
@@ -614,6 +621,10 @@ class I18nl10n extends \Controller
             $strTitle = $objNextPage->pageTitle ?: $objNextPage->title;
 
             return sprintf('<a href="%s" title="%s"%s>%s</a>', $strUrl, specialchars($strTitle), $strTarget, specialchars($strName));
+        }
+        elseif ($arrArguments[1] === 'link_name')
+        {
+	        return $objNextPage->title;
         }
 
         return false;
